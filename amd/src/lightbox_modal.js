d77ff62aa996d64d06921d0e03ee1a85309ed575
John Lane <46030891+JohnOLane@users.noreply.github.com>
2025-01-21 15:03:23 -0600

    Lightbox modal.

---
+++ amd/src/lightbox_modal.js
@@ -0,0 +1,6 @@
+import Modal from 'core/modal';
+
+export default class LightboxModal extends Modal {
+    static TYPE = 'theme_urcourses/lightbox_modal';
+    static TEMPLATE = 'theme_urcourses/lightbox_modal';
+}
